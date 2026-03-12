d6c2745a0d0786d54a9d83e398d665c47ecc27a5
BlooperSP <Thebloopersp@gmail.com>
2026-03-13 03:05:46 +0530

    Register service route

--- backend/server.js
+++ backend/server.js
@@ -104,6 +104,7 @@ app.use('/api/reviews', reviewRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/invites', inviteRoutes);
 app.use('/api/payments', paymentRoutes);
+app.use('/api/services', serviceRoutes);
 
 
 // Create tables function
